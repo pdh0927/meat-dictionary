d357d896278fac51062d28e824295a972f82acd8
dh <pdh980927@naver.com>
2024-06-27 15:22:50 +0900

    cleanup: create const color

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const PRIMARY_COLOR = Color(0xFF9D2F2F); // 주 색상
+const BLACK_COLOR = Color(0xFF131313); // 색상(검정)
+const GREY_COLOR = Color(0xFF1A1A1A); // 색상(회색)
