972d5e8f31a1eeb2349b0f107d53a7f5c9c7bd26
dh <pdh980927@naver.com>
2024-08-09 23:36:59 +0900

    feat: create home screen

--- lib/common/view/home_screen.dart
+++ lib/common/view/home_screen.dart
@@ -1,14 +1,91 @@
 import 'package:flutter/material.dart';
-import 'package:meat_dictionary/meat/component/dictionary.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meat_dictionary/common/component/meat_search_delegate.dart';
+import 'package:meat_dictionary/common/layout/default_layout.dart';
+import 'package:meat_dictionary/meat/component/dictionary_list.dart';
+import 'package:meat_dictionary/meat/component/meat_menu.dart';
+import 'package:sizer/sizer.dart';
 
-class HomeScreen extends StatelessWidget {
-
+class HomeScreen extends ConsumerStatefulWidget {
   static String get routeName => 'home';
 
   const HomeScreen({super.key});
 
   @override
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  // 기본값을 돼지로 설정
+  String selectedType = 'pork';
+
+  void _onTypeChanged(String type) {
+    setState(() {
+      selectedType = type;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Column(children: [Dictionary()],);
+    return DefaultLayout(
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              // 로고 이미지
+              Image.asset(
+                'assets/imgs/common/logo2.png',
+                width: 35.w,
+                height: 15.w,
+                fit: BoxFit.fill,
+              ),
+
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xFFF3F4F8), // 검색창 배경색
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: Icon(Icons.search, color: Colors.grey),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        showSearch(
+                          context: context,
+                          delegate: MeatSearchDelegate(ref: ref),
+                        );
+                      },
+                      child: const Text(
+                        '고기 이름을 검색해보세요!',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: Color(0XFF9496A1), // 힌트 텍스트 색상
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              MeatMenu(
+                selectedType: selectedType,
+                onTypeChanged: _onTypeChanged,
+              ),
+              const SizedBox(height: 24.0),
+              Expanded(
+                child: DictionaryList(
+                  isFavoritesScreen: false,
+                  type: selectedType,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/meat/component/dictionary_list.dart
+++ lib/meat/component/dictionary_list.dart
@@ -19,37 +19,35 @@ class DictionaryList extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final favoriteMap = ref.watch(favoritesProvider);
     final favoriteIds = favoriteMap[type] ?? {};
+    final selectedList = type == 'pork' ? porkList : beefList;
 
     // 즐겨찾기 화면인 경우 즐겨찾기 항목만 필터링
     final filteredList = isFavoritesScreen
-        ? porkList
+        ? selectedList
             .where((meat) =>
                 favoriteIds.contains(meat.id) &&
                 (type == null || meat.type == type))
             .toList()
-        : porkList;
+        : selectedList;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: ListView.builder(
-        itemCount: filteredList.length,
-        itemBuilder: (context, index) {
-          final meat = filteredList[index];
-          final isSelected = favoriteIds.contains(meat.id);
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: InkWell(
-              onTap: () {
-                context.pushNamed("meat_detail");
-              },
-              child: DictionaryListComponent(
-                meatModel: meat,
-                isSelected: isSelected,
-              ),
+    return ListView.builder(
+      itemCount: filteredList.length,
+      itemBuilder: (context, index) {
+        final meat = filteredList[index];
+        final isSelected = favoriteIds.contains(meat.id);
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: InkWell(
+            onTap: () {
+              context.pushNamed("meat_detail");
+            },
+            child: DictionaryListComponent(
+              meatModel: meat,
+              isSelected: isSelected,
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
