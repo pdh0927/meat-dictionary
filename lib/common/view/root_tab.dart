25a1975e5064e3945fe888bfad044b32a0fb6118
dh <pdh980927@naver.com>
2024-10-16 17:02:40 +0900

    feat: add 3 tabs

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meat_dictionary/card_news/view/card_news_screen.dart';
 import 'package:meat_dictionary/common/const/colors.dart';
 import 'package:meat_dictionary/common/layout/default_layout.dart';
 import 'package:meat_dictionary/common/provider/root_tab_index_provider.dart';
+import 'package:meat_dictionary/common/view/custom_search_screen.dart';
 import 'package:meat_dictionary/common/view/home_screen.dart';
+import 'package:meat_dictionary/meat/model/meat_model.dart';
 
 import 'package:meat_dictionary/meat/view/favorites_screen.dart';
+import 'package:meat_dictionary/meat/view/meat_list_screen.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 class RootTab extends ConsumerStatefulWidget {
@@ -25,7 +29,7 @@ class _RootTabState extends ConsumerState<RootTab>
   void initState() {
     super.initState();
     // length : 몇 개 탭 사용할건지, vsync : 현재의  state 넣어주면 됨(그런데 SingleTickerProviderStateMixin이라는 기능 가지고 있어야함)
-    controller = TabController(length: 2, vsync: this);
+    controller = TabController(length: 5, vsync: this);
     // controller의 변경사항이 감지될 때마다 tabListener 함수 실행
     controller.addListener(tabListener);
   }
@@ -70,20 +74,48 @@ class _RootTabState extends ConsumerState<RootTab>
           BottomNavigationBarItem(
             icon: Icon(
               ref.watch(rootTabIndexProvider) == 1
-                  ? PhosphorIconsFill.star
-                  : PhosphorIcons.star(),
+                  ? PhosphorIconsFill.magnifyingGlass
+                  : PhosphorIcons.magnifyingGlass(),
+            ),
+            label: '검색',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              ref.watch(rootTabIndexProvider) == 2
+                  ? PhosphorIconsFill.book
+                  : PhosphorIcons.book(),
+            ),
+            label: '도감',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              ref.watch(rootTabIndexProvider) == 3
+                  ? PhosphorIconsFill.newspaper
+                  : PhosphorIcons.newspaper(),
+            ),
+            label: '카드뉴스',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              ref.watch(rootTabIndexProvider) == 4
+                  ? PhosphorIconsFill.heart
+                  : PhosphorIcons.heart(),
             ),
             label: '즐겨찾기',
           ),
         ],
       ),
       child: TabBarView(
-          physics: const NeverScrollableScrollPhysics(), // scroll로는 화면 전환 x
-          controller: controller,
-          children: const [
-            HomeScreen(),
-            FavoritesScreen(),
-          ]),
+        physics: const NeverScrollableScrollPhysics(), // scroll로는 화면 전환 x
+        controller: controller,
+        children: const [
+          HomeScreen(),
+          CustomSearchScreen(),
+          MeatListScreen(meatType: MeatType.pork),
+          CardNewsScreen(),
+          FavoritesScreen(),
+        ],
+      ),
     );
   }
 }
