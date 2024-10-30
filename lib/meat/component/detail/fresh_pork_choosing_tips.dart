7d147261a0ace00cb51a9e1e416252786b48d726
dh <pdh980927@naver.com>
2024-10-30 17:52:24 +0900

    feat: change description contents

--- lib/meat/component/detail/fresh_pork_choosing_tips.dart
+++ lib/meat/component/detail/fresh_pork_choosing_tips.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meat_dictionary/meat/component/detail/horizozntal_images.dart';
 import 'package:meat_dictionary/meat/component/detail/small_title_components.dart';
 
-// 신선한 고기 고르는 방법
+// 신선한 돼지고기 고르는 방법
 class FreshPorkChoosingTips extends StatefulWidget {
   const FreshPorkChoosingTips({super.key});
 
@@ -27,18 +27,18 @@ class _FreshPorkChoosingTipsState extends State<FreshPorkChoosingTips> {
 
   final List<String> titles = const [
     '흐물하지 않고 탄력 있는 것',
-    '고기색이 분홍색을 띠는 것',
+    '고기가 맑은 분홍색을 띠는 것',
     '지방이 희고 단단한 것',
     '겉에 핏물이 없는 것',
   ];
 
   // 강조 문자열
-  final List<String> highlights = const ['탄력', '분홍', '희고 단단', '핏물'];
+  final List<String> highlights = const ['탄력', '분맑은 분홍색홍', '희고 단단', '핏물'];
 
   // 추가 설명
   final List<String> descriptions = const [
     '탄력이 있을수록 신선해요.',
-    '색이 희미하면 신선하지 않아요.',
+    '색이 탁할수록 신선하지 않아요.',
     '지방이 누런건 오래된 고기예요.',
     '핏물이 나오지 않았을수록 신선해요.',
   ];
@@ -71,16 +71,16 @@ class _FreshPorkChoosingTipsState extends State<FreshPorkChoosingTips> {
             goodImageUrls: goodImageUrls,
             badImageUrls: badImageUrls,
             goodDescriptionsList: const [
-              ['근육이 많이 갈라진 것', '하나의 근육이 아닌 것'],
-              ['흰색 근내 지방이 많은 것', '살코기가 안 많은 것'],
-              ['흰색 근내 지방이 많은 것', '살코기가 안 많은 것'],
-              ['흰색 근내 지방이 많은 것', '살코기가 안 많은 것'],
+              ['흐물하지 않고 탄력이 있는 것', '눌렀을 때 원상복귀가 빠른 것'],
+              ['고기가 맑은 분홍색을 띠는 것'],
+              ['지방이 흰색인 것', '지방이 단단한 것'],
+              ['겉에 핏물이 없는 것'],
             ],
             badDescriptionsList: const [
-              ['근육이 많이 갈라진 것', '하나의 근육이 아닌 것'],
-              ['흰색 근내 지방이 많은 것', '살코기가 안 많은 것'],
-              ['흰색 근내 지방이 많은 것', '살코기가 안 많은 것'],
-              ['흰색 근내 지방이 많은 것', '살코기가 안 많은 것'],
+              ['고기가 흐물흐물한 것', '눌렀을 때 천천히 돌아오는 것'],
+              ['고기가 회색인 것', '고기가 갈변된 것'],
+              ['지방이 누런색 인 것', '지방이 흐물흐물한 것'],
+              ['겉에 핏물이 많이 나온 것'],
             ],
           )
         ],
